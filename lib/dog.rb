52fe657bb0d8a259c2e2213761e24c13a9ef8e71
mariesonko <mariesonko62@gmail.com>
2018-04-25 00:45:05 -0400

    created dog and person class and define bark, sit , talk and walk

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+Class Dog
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The dog is sitting"
+  end
+
+lulu = Dog.new
+lulu.bark
+lulu.sit
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+Class Person
+
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The person is walking"
+  end
+
+end
+
+
+alan = Person.new
+alan.talk
+alan.walking
